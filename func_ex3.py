8dd71f1d8a6bdb7dcd09563e661d5aab3362ef99
USHNISH SARKAR <117502402+UshnishRivu14@users.noreply.github.com>
2023-08-30 21:33:24 +0530

    Add files via upload
    Return multiple values from a function.

---
+++ func_ex3.py
@@ -0,0 +1,14 @@
+def calculation(a,b):
+    c = a +b
+    d = a - b
+    return c, d
+
+print("first value: ")
+res1 = calculation(3,2)
+
+print(res1)
+
+print("first value: ")
+res2 = calculation(6,3)
+
+print(res2)
